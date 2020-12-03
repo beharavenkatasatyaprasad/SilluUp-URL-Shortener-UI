5954b36246400523a020394fe83bea1ab0bab2e8
beharavenkatasatyaprasad <satyaprasadbehara@gmail.com>
2020-12-03 16:11:02 +0530

    commit

--- js/common.js
+++ js/common.js
@@ -10,4 +10,12 @@ document.getElementById('admin').addEventListener('click', () => {
     window.location.href = "../admin.html";
 });
 
+if(!navigator.cookieEnabled){
+    let newAlert = $("#message");
+    message = 'This site uses cookies in order to ensure secure login and logout. Please enable cookies in your browser and comeback..'
+    newAlert.html(`
+        <div class="fade-in text-center m-0 alert alert-warning fade show" role="alert">
+            <i class="fa fa-exclamation-circle alert-warning" aria-hidden="true"></i> ${message}
+        </div>`);
+}
 
